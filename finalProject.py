29b1e0aba69db661eaeddcca9187535bec034e01
ale-111 <73674315+ale-111@users.noreply.github.com>
2021-01-21 10:01:33 +0700

    Work session 6

--- finalProject.py
+++ finalProject.py
@@ -476,3 +476,35 @@ for i in words:
 
 print(c)
 
+### Problem 18
+ 
+pyr = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+
+pyr = pyr.strip().split('\n') ### convert the pyramid into a list
+
+for i in range(0,len(pyr)):  ### convert num to str for operation
+	pyr[i] = pyr[i].strip().split(' ')
+	pyr[i] = [int(x) for x in pyr[i]]
+
+c = 0
+for i in range(len(pyr)-2, -1, -1): ### find max sum from bottom 
+    for x in range(len(pyr[i])):
+        pyr[i][x] =  pyr[i][x] + max(pyr[i+1][x], pyr[i+1][x+1])
+        c += 1
+    pyr.pop()
+
+print(pyr[0][0])
